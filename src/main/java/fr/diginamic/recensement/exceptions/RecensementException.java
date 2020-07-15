b6f916f5e75bc3a7af2841464c64073953198578
“[pablodacampo]” <[pab.c@programmer.net]>
2020-07-15 10:06:21 +0200

    V2

--- src/main/java/fr/diginamic/recensement/exceptions/RecensementException.java
+++ src/main/java/fr/diginamic/recensement/exceptions/RecensementException.java
@@ -5,8 +5,4 @@ public class RecensementException extends Exception {
 	public RecensementException(String message) {
 		super(message);
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ef9714bdf3e2f69c51883f19668fa95b5877c7ba
